d450c486a1d3be48c4972fcf4fbb181a68ec550a
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-03-11 14:08:03 -0300

    Create Exercício02.c

---
+++ Exercício02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+float n1;
+
+n1=0;
+
+printf("\nEscreva um numero real:");
+scanf("%f", &n1);
+printf("\nO numero digitado foi: %f", n1);
+system("pause");
+
+return(0);
+}
