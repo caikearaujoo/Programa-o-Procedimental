048089d2855e23089ef7efea54063b6fa59aede6
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-03-11 13:45:31 -0300

    Create Exercício01.c

---
+++ Exercício01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+int n1;
+
+n1=0;
+
+printf("\nEscreva um numero inteiro:");
+scanf("%d", &n1);
+printf("\nO numero digitado foi: %d", n1);
+
+system("pause");
+return (0);
+
+}
