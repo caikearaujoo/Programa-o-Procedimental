284f121e7c90220f94a78aded7cbbd0c86d6b83f
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-03-19 16:04:39 -0300

    Update Exercicio53.c

--- Exercicio53.c
+++ Exercicio53.c
@@ -1,20 +1,17 @@
 
 int main() {
-    float comprimento, largura, preco_tela, custo;
+    float c, l, p, custo;
     
-    // Lendo as dimensões do terreno e o preço da tela
-    printf("Digite o comprimento do terreno (em metros): ");
-    scanf("%f", &comprimento);
-    printf("Digite a largura do terreno (em metros): ");
-    scanf("%f", &largura);
+    printf("Digite o comprimento do terreno em metros: ");
+    scanf("%f", &c);
+    printf("Digite a largura do terreno em metros: ");
+    scanf("%f", &l);
     printf("Digite o preco do metro de tela (em reais): ");
-    scanf("%f", &preco_tela);
+    scanf("%f", &p);
     
-    // Calculando o custo da tela
-    custo = 2 * (comprimento + largura) * preco_tela;
-    
-    // Imprimindo o resultado
-    printf("O custo para cercar o terreno com tela sera de R$ %.2f\n", custo);
+    custo = 2 * (c + l) * p;
+
+    printf("O custo para cercar o terreno com tela sera de R$ %.2f\n", c);
     
     return 0;
 }
