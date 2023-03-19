0f7d11802e2a4a8a56409df90eea94b305a6ce99
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-03-19 15:52:16 -0300

    Update Exercício51.c

--- Exercício51.c
+++ Exercício51.c
@@ -4,16 +4,14 @@
 int main() {
     float x, y, distancia;
     
-    // Lendo as coordenadas x e y
     printf("Digite a coordenada x: ");
     scanf("%f", &x);
     printf("Digite a coordenada y: ");
     scanf("%f", &y);
     
-    // Calculando a distância
     distancia = sqrt(x * x + y * y);
     
-    // Imprimindo o resultado
-    printf("A distancia do ponto (%.2f, %.2f) da origem (0, 0) e %.2f\n", x, y, distancia);
-    
+    printf("A distancia do ponto (%.2f, %.2f) da origem (0, 0) e %f.\n", x, y, distancia);
+
     return 0;
+}
