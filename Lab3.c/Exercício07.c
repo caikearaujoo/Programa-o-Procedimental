08f2a33bf3f5fed9120a8340b5226461b5463295
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-03-31 16:04:25 -0300

    Update Exercício07.c

--- Lab3.c/Exercício07.c
+++ Lab3.c/Exercício07.c
@@ -8,19 +8,22 @@ float media=0;
 int qntd=0;
 int valor=0;
 int soma=0;
-int qntd_negativo=0;
 while (qntd<10)
     {
-    printf("\nDigite um numero inteiro:\n");
-    scanf ("%d", &valor);
-    if (valor<0)
+    printf("\nDigite um numero inteiro positivo:\n");
+    scanf("%d", &valor);
+    if (valor>=0) 
     {
-        qntd_negativo++;
-    }
     qntd++;
     soma += valor;
-}
-    media = soma/(qntd-qntd_negativo);
-printf("\nA media dos valores eh: %f",media);
+    }
+    else 
+    { 
+        continue;
+    }
+    }
+    media = soma/(qntd);
+printf("\nA media dos valores positivos eh: %f",media);
 return 0;
 }
+
