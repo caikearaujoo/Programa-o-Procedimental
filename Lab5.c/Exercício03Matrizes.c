4141e35f19c1a6cd399851603f7e9db92924874f
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-05-02 11:17:28 -0300

    Update Exercício03Matrizes.c

--- Lab5.c/Exercício03Matrizes.c
+++ Lab5.c/Exercício03Matrizes.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
+#include <math.h>
 
-int main() {
-  int matriz[4][4];
-  int maior_valor = matriz[0][0];
-  int linha_maior, coluna_maior;
+int main()
+{
 
-  printf("Digite os valores que deseja colocar na matriz 4x4:\n");
-  for(int i = 0; i < 4; i++) 
-  {
-    for(int j = 0; j < 4; j++) 
+    int mat[10][10];
+    int i,j,quadrado_i,quadrado_j,cubo_i;
+
+    for(i=0;i<10;i++)
     {
-      scanf("%d", &matriz[i][j]);
-      if(matriz[i][j] > maior_valor) 
-      { 
-        maior_valor = matriz[i][j];
-        linha_maior = i;
-        coluna_maior = j;
-      }
+        for(j=0;j<10;j++)
+        {
+            if(i<j)
+            {
+                mat[i][j] = (2*i)+(7*j);
+            } else 
+            {
+                if(i==j)
+                {
+                    quadrado_i= pow (i,2);
+                    mat[i][j]=(3*quadrado_i) - 1;
+                } else {
+                    if(i>j){
+                        quadrado_j= pow (j,2);
+                        cubo_i= pow (i,3);
+                        mat[i][j]= (4*cubo_i) - (5*quadrado_j);
+                    }
+                }
+            }
+        }
     }
-  }
 
-  printf("Matriz lida:\n");
-  for(int i = 0; i < 4; i++) 
-  {
-    for(int j = 0; j < 4; j++) 
-    {
-      printf("%d ", matriz[i][j]);
+    for(i=0;i<10;i++){
+        for(j=0;j<10;j++){
+            printf("%d ",mat[i][j]);
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
-
-  printf("Maior valor encontrado: %d\n", maior_valor);
-  printf("Localizacao: linha %d, coluna %d\n", linha_maior, coluna_maior);
-
-  return 0;
+    
+    return 0;
 }
