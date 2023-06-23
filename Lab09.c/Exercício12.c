d32e5bbb8b213775f303f4a21e5279558db33cbe
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-06-22 22:23:17 -0300

    Update Exercício12.c

--- Lab09.c/Exercício12.c
+++ Lab09.c/Exercício12.c
@@ -1,44 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct produto
-{
+typedef struct {
     int codigo;
     char nome[50];
     int quantidade;
     float preco;
-}; typedef struct produto produto;
+} Produto;
 
-int main() 
-{
-    int num_produtos;
+int main() {
+    int numProdutos;
     int i;
+    int maxPrecoIndex = 0;
+    int maxQuantidadeIndex = 0;
 
     printf("Digite o número de produtos: ");
-    scanf("%d", &num_produtos);
+    scanf("%d", &numProdutos);
 
-    produto *estoque = (produto *) malloc(num_produtos * sizeof(produto));
+    Produto *estoque = (Produto *) malloc(numProdutos * sizeof(Produto));
 
-    for (i = 0; i < num_produtos; i++) {
+    for (i = 0; i < numProdutos; i++) {
         printf("\nProduto %d:\n", i+1);
-        printf("Codigo: ");
+        printf("Código: ");
         scanf("%d", &estoque[i].codigo);
         printf("Nome: ");
-        scanf("%s", estoque[i].nome); 
+        scanf(" %s", estoque[i].nome);
         printf("Quantidade: ");
         scanf("%d", &estoque[i].quantidade);
-        printf("Preco: ");
+        printf("Preço: ");
         scanf("%f", &estoque[i].preco);
+
+
+        if (estoque[i].preco > estoque[maxPrecoIndex].preco) {
+            maxPrecoIndex = i;
+        }
+
+        if (estoque[i].quantidade > estoque[maxQuantidadeIndex].quantidade) {
+            maxQuantidadeIndex = i;
+        }
     }
 
     printf("\nProdutos cadastrados:\n");
-    for (i = 0; i < num_produtos; i++) {
+    for (i = 0; i < numProdutos; i++) {
         printf("\nProduto %d:\n", i+1);
-        printf("Codigo: %d\n", estoque[i].codigo);
+        printf("Código: %d\n", estoque[i].codigo);
         printf("Nome: %s\n", estoque[i].nome);
         printf("Quantidade: %d\n", estoque[i].quantidade);
-        printf("Preco: %.2f\n", estoque[i].preco);
+        printf("Preço: %.2f\n", estoque[i].preco);
     }
+
+    printf("\nProduto com o maior preço de venda:\n");
+    printf("Código: %d\n", estoque[maxPrecoIndex].codigo);
+    printf("Nome: %s\n", estoque[maxPrecoIndex].nome);
+    printf("Quantidade: %d\n", estoque[maxPrecoIndex].quantidade);
+    printf("Preço: %.2f\n", estoque[maxPrecoIndex].preco);
+
+    printf("\nProduto com a maior quantidade disponível no estoque:\n");
+    printf("Código: %d\n", estoque[maxQuantidadeIndex].codigo);
+    printf("Nome: %s\n", estoque[maxQuantidadeIndex].nome);
+    printf("Quantidade: %d\n", estoque[maxQuantidadeIndex].quantidade);
+    printf("Preço: %.2f\n", estoque[maxQuantidadeIndex].preco);
 
     free(estoque);
 
