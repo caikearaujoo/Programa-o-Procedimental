63ebd90ebdab8a194a067740576e3867266d1eb8
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-06-25 19:21:56 -0300

    Update Exercício01.c

--- Exercícios_de_Fixacao/Exercício01.c
+++ Exercícios_de_Fixacao/Exercício01.c
@@ -1,43 +1,41 @@
 #include <stdio.h>
 
-void pontuacao (int *p1, int *p2, int *g1, int *g2);
+void pontuacao (int *pontosA, int *pontosB, int golsA, int golsB);
 
 int main ()
 {
     int pontosA, pontosB, golsA, golsB;
-    int *p1 = NULL, *p2 = NULL, *g1 = NULL, *g2 = NULL;
-    p1 = &pontosA, p2 = &pontosB, g1 = &golsA, g2 = &golsB;
 
-    printf("\nDigite a pontuacao inicial do time A: ");
-    scanf("%d", p1);
-    printf("\nDiite a pontuacao inicial do time B: ");
-    scanf("%d", p2);
-    
-    printf("\nDigite a quantidade de gols que o time A fez: ");
-    scanf ("%d", g1);
-    printf("\nDigite a quantidade de gols que o time B fez: ");
-    scanf ("%d", g2);
+    printf("\nDigite quantos pontos o time A possui: ");
+    scanf("%d", &pontosA);
+    printf("\nDigite quantos pontos o time B possui: ");
+    scanf("%d", &pontosB);
+    printf("\nDigite quantos gols o time A fez na partida: ");
+    scanf("%d", &golsA);
+    printf("\nDigite quantos gols o time B fez na partida: ");
+    scanf("%d", &golsB);
 
-    pontuacao (p1, p2, g1, g2);
-
+    pontuacao (&pontosA, &pontosB, golsA, golsB);
     return 0;
+
 }
 
-void pontuacao (int *p1, int *p2, int *g1, int *g2)
+void pontuacao (int *pontosA, int *pontosB, int golsA, int golsB)
 {
-    if (*g1 == *g2)
+    if (golsA > golsB)
     {
-        *p1 += 1;
-        printf("\nOs times empataram e seus pontos atuais sao: %d do time A e %d do time B ", *p1, *p2 );
+        *pontosA += 3;
+        printf("\nO time A venceu! Sua nova pontuacao eh: %d", *pontosA);
     }
-    else if (*g1 > *g2)
+    if (golsA == golsB)
     {
-        *p1 += 3;
-        printf("\nO time A venceu e seus pontos atuais sao: %d", *p1);
+        *pontosA += 1;
+        *pontosB += 1;
+        printf("\nO jogo deu empate! A nova pontuacao do time A eh %d e do time B eh %d", *pontosA, *pontosB);
     }
-    else if (*g2 > *g1)
+    if (golsB > golsA)
     {
-        *p2 += 3;
-        printf("\nO time B venceu e seus pontos atuais sao: %d", *p2);
+        *pontosB += 3;
+        printf("\nO time B venceu! Sua nova pontuacao eh: %d", *pontosB);
     }
 }
