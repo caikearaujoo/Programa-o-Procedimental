00067080d7277ff5ffde1c065c196e4fc243f2a9
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-06-25 20:44:00 -0300

    Update Exercício06.c

--- Exercícios_de_Fixacao/Exercício06.c
+++ Exercícios_de_Fixacao/Exercício06.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 int main ()
 {
     FILE *fp;
-    char nome[20];
+    char nome[30];
     int nota1, nota2, nota3, media;
-    
+
     fp = fopen ("notas.txt", "r");
-    if (fp == NULL)
-    {
-        printf("\nErro ao abrir o arquivo.");
-    }
     while (fscanf(fp, "%s %d %d %d", nome, &nota1, &nota2, &nota3) == 4)
     {
         media = (nota1 + nota2 + nota3)/3;
         if (media >= 6)
         {
-            printf("\n%s: %d -- APROVADO", nome, media);
+            printf("\n%s: %d --- ALUNO APROVADO", nome, media);
         }
-        else
+        else 
         {
-            printf("\n%s: %d -- REPROVADO", nome, media);
+            printf("\n%s: %d --- ALUNO REPROVADO", nome, media);
         }
     }
-    fclose(fp);
+
+    fclose (fp);
     return 0;
 }
