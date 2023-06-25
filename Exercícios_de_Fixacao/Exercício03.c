7ce75e181bd334b0099dadad4e98da2b22eebf2b
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-06-25 19:53:41 -0300

    Update Exercício03.c

--- Exercícios_de_Fixacao/Exercício03.c
+++ Exercícios_de_Fixacao/Exercício03.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-int main()
+int main ()
 {
-    int vet[10], i;
+    int vet [10], i;
 
-    for (i=0; i<10; i++)
+    for (i = 0; i < 10; i++)
     {
-        printf("\nDigite o valor da posicao %d do vetor: ", i);
-        scanf ("%d", &vet[i]);
+        printf("\nDigite um valor para a posicao %d do vetor: ", i);
+        scanf("%d", &*(vet+i));
     }
 
     printf("\n[");
     for (i = 9; i >= 0; i--)
     {
-        printf( " %d ", *(vet+i));
+        printf(" %d ", *(vet+i)); 
     }
     printf("]");
+
     return 0;
 }
